6c6baeaa32328066668c4adaaa9004842230c42a
kodakk <20fi039@gmail.com>
2023-03-22 16:00:11 +0900

    仮のhtmlをindexdで開けるようにした

--- MySNS/MyApp/urls.py
+++ MySNS/MyApp/urls.py
@@ -2,13 +2,21 @@ from django.urls import path
 from django.views.generic import RedirectView
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import UserPostListView
+from .views import UserPostListView, TalkView, PostView, MyPageView
+
+app_name = 'MyApp'
 
 urlpatterns = [
     path('', RedirectView.as_view(url='index/', permanent=True)),
+
     path('index/', UserPostListView.as_view(), name="index"),
+    path('talk/', TalkView.as_view(), name="talk"),
+    path('post/', PostView.as_view(), name="post"),
+    path('myPage/', MyPageView.as_view(), name="myPage"),
+
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='MyApp:login'), name='logout'),
+
     # path("detail/<int:pk>", detailfunc, name='detail'),
     # path("good/<int:pk>", goodfunc, name='good'),
     # path("read/<int:pk>", readfunc, name='read'),
--- MySNS/MyApp/views.py
+++ MySNS/MyApp/views.py
@@ -13,3 +13,32 @@ class UserPostListView(ListView):
         # return Post.objects.filter(author_id=author_id)
         return Post.objects.filter()
 
+class TalkView(ListView):
+    model = Post
+    template_name = 'talk.html'
+    # context_object_name = 'posts'
+    
+    # def get_queryset(self):
+    #     author_id = self.kwargs.get('author_id')
+    #     # return Post.objects.filter(author_id=author_id)
+    #     return Post.objects.filter()
+
+class PostView(ListView):
+    model = Post
+    template_name = 'post.html'
+    # context_object_name = 'posts'
+    
+    # def get_queryset(self):
+    #     author_id = self.kwargs.get('author_id')
+    #     # return Post.objects.filter(author_id=author_id)
+    #     return Post.objects.filter()
+
+class MyPageView(ListView):
+    model = Post
+    template_name = 'myPage.html'
+    # context_object_name = 'posts'
+    
+    # def get_queryset(self):
+    #     author_id = self.kwargs.get('author_id')
+    #     # return Post.objects.filter(author_id=author_id)
+    #     return Post.objects.filter()

370b318ebb25c962c488b50827a451b7b1538ecb
kodakk <20fi039@gmail.com>
2023-03-22 16:00:41 +0900

    settingのログインリダイレクトを変更

--- MySNS/MySNS/settings.py
+++ MySNS/MySNS/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/MyApp/static/'
+STATIC_URL = '/myapp/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'MyApp', 'static'),
@@ -122,4 +122,4 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-LOGIN_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = 'MyApp:index'

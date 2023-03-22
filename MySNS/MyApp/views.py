9ca47f11960021f2acbf1bae2772377bddadc0ae
kodakk <20fi039@gmail.com>
2023-03-22 14:20:30 +0900

    ListViewを継承したクラスを作成 Postモデルを返す

--- MySNS/MyApp/views.py
+++ MySNS/MyApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+from .models import Post
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'posts'
+    
+    def get_queryset(self):
+        author_id = self.kwargs.get('author_id')
+        # return Post.objects.filter(author_id=author_id)
+        return Post.objects.filter()
+

ae076b3af483aa97474e85e67d4f6e116f91d1de
kodakk <20fi039@gmail.com>
2023-03-24 20:05:25 +0900

    Postモデルの変更 titleとcontentに文字数制限 __str__メソッド追加 Metaクラスで名前変えた

--- MySNS/MyApp/models.py
+++ MySNS/MyApp/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=1000)
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "投稿"
+        verbose_name_plural = "投稿一覧"

9f446cef3727ad6c2e2f244ad2796f68e554e24c
kodakk <20fi039@gmail.com>
2023-03-24 21:19:13 +0900

    models.pyの編集 Postモデルにカテゴリ、画像のフィールドを作成 Likeクラスでいいね、Readクラスで既読を管理

--- MySNS/MyApp/models.py
+++ MySNS/MyApp/models.py
@@ -2,10 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
+    CATEGORY_CHOICES = (
+        ('AI', 'AI'),
+        ('Python', 'Python'),
+        ('java', 'java'),
+    )
+
     title = models.CharField(max_length=50)
     content = models.TextField(max_length=1000)
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='Tech')
 
     def __str__(self):
         return self.title
@@ -13,3 +21,11 @@ class Post(models.Model):
     class Meta:
         verbose_name = "投稿"
         verbose_name_plural = "投稿一覧"
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Read(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)

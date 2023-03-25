6d5c979119b0f42fa603ee8a12a508c2f1cf8971
kodakk <20fi039@gmail.com>
2023-03-25 20:42:35 +0900

    talkをtalksとしてhtmlで使用できるようにした。 さっきはできてなかった

--- MySNS/MyApp/views.py
+++ MySNS/MyApp/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 
 from django.views.generic import ListView, CreateView
-from .models import Post, Talk
+from .models import Post, PostLike, Talk
 from django.db.models import Count
+
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 
 class UserPostListView(ListView):
     model = Post
@@ -17,7 +21,7 @@ class UserPostListView(ListView):
 class TalkView(ListView):
     model = Talk
     template_name = 'talk.html'
-    # context_object_name = 'posts'
+    context_object_name = 'talks'
     
     # def get_queryset(self):
     #     author_id = self.kwargs.get('author_id')
@@ -56,3 +60,22 @@ class MyPageView(ListView):
     #     author_id = self.kwargs.get('author_id')
     #     # return Post.objects.filter(author_id=author_id)
     #     return Post.objects.filter()
+
+@require_POST
+@login_required
+def like(request):
+    post_id = request.POST.get('post_id')
+    post = Post.objects.get(id=post_id)
+    user = request.user
+    like = PostLike.objects.filter(post=post, user=user)
+    liked = False
+    if like:
+        like.delete()
+    else:
+        PostLike.objects.create(post=post, user=user)
+        liked = True
+    context = {
+        'liked': liked,
+        'count': post.postlike_set.count()
+    }
+    return JsonResponse(context)

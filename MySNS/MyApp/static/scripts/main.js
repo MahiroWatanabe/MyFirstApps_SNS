b81fc6cfa8da1ffd1f4858432eab640b8c8cd761
MahiroWatanabe <konchikuwa2@gmail.com>
2023-03-25 16:48:14 +0900

    post.htmlとtalk.htmlの編集

--- MySNS/MyApp/static/scripts/main.js
+++ MySNS/MyApp/static/scripts/main.js
@@ -5,3 +5,11 @@ document.addEventListener('DOMContentLoaded', function () {
     new MobileMenu;
 });
 
+function searchOnEnter(event) {
+  if (event.keyCode === 13) { // Enterキーが押された場合
+    var searchText = event.target.value; // inputタグの値を取得
+    // ここで検索処理を実行する
+    return false; // フォームの送信をキャンセルする
+  }
+  return true;
+}

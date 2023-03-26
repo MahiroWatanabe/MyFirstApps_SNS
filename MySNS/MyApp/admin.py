9606193562c1135de224c5ccd199ee05c4a92b6d
kodakk <20fi039@gmail.com>
2023-03-26 23:25:05 +0900

    PostLikeの編集権限

--- MySNS/MyApp/admin.py
+++ MySNS/MyApp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Post, Talk
+from .models import Post, Talk, PostLike
 
 admin.site.register(Post)
 
 admin.site.register(Talk)
+
+admin.site.register(PostLike)

2555a361c997ccfa4dcfc38db155bb2833491970
kodakk <20fi039@gmail.com>
2023-03-30 23:27:52 +0900

    talk_likeメソッドを追加 トークでのいいね機能を実装

--- MySNS/MyApp/views.py
+++ MySNS/MyApp/views.py
@@ -14,7 +14,7 @@ from django.http import JsonResponse
 
 from django.views.decorators.http import require_POST
 
-from .models import Post, PostLike, Talk
+from .models import Post, PostLike, Talk, TalkLike
 from .forms import CustomUserCreationForm
 
 class UserPostListView(ListView):
@@ -26,46 +26,6 @@ class UserPostListView(ListView):
         author_id = self.kwargs.get('author_id')
         # return Post.objects.filter(author_id=author_id)
         return Post.objects.filter()
-
-#-----Talk-------
-
-class TalkView(ListView):
-    model = Talk
-    template_name = 'talk.html'
-    context_object_name = 'talks'
-    
-    # def get_queryset(self):
-    #     author_id = self.kwargs.get('author_id')
-    #     # return Post.objects.filter(author_id=author_id)
-    #     return Post.objects.filter()
-
-class TalkDetailView(DetailView):
-    model = Talk
-    template_name = 'talkDetail.html'
-    context_object_name = 'talk'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        talklike_count = self.object.talklike_set.count()
-        # ポストに対するイイね数
-        context['talklike_count'] = talklike_count
-        # ログイン中のユーザーがイイねしているかどうか
-        if self.object.talklike_set.filter(user=self.request.user).exists():
-            context['is_user_liked_for_talk'] = True
-        else:
-            context['is_user_liked_for_talk'] = False
-
-        return context
-
-class TalkCreateView(LoginRequiredMixin, CreateView):
-    model = Talk
-    fields = ['content']
-    template_name = 'talk_create.html'
-    success_url = reverse_lazy('MyApp:talk')
-    
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
 
 #------Post-----
 
@@ -110,28 +70,6 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class MyPageView(LoginRequiredMixin, ListView):
-    model = Post
-    template_name = 'myPage.html'
-    context_object_name = 'myPosts'
-    
-    def get_queryset(self):
-        return Post.objects.filter(author=self.request.user)
-
-class SignupView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('MyApp:index')
-    template_name = 'signup.html'
-
-    def form_valid(self, form):
-        response = super().form_valid(form)
-        # ユーザーが作成された場合はログインする
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=password)
-        login(self.request, user)
-        return response
-    
 @require_POST
 @login_required
 def post_like(request):
@@ -153,6 +91,90 @@ def post_like(request):
 
     return JsonResponse(context)
 
+#-----Talk-------
+
+class TalkView(ListView):
+    model = Talk
+    template_name = 'talk.html'
+    context_object_name = 'talks'
+    
+    # def get_queryset(self):
+    #     author_id = self.kwargs.get('author_id')
+    #     # return Post.objects.filter(author_id=author_id)
+    #     return Post.objects.filter()
+
+class TalkDetailView(DetailView):
+    model = Talk
+    template_name = 'talkDetail.html'
+    context_object_name = 'talk'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        talklike_count = self.object.talklike_set.count()
+        # ポストに対するイイね数
+        context['talklike_count'] = talklike_count
+        # ログイン中のユーザーがイイねしているかどうか
+        if self.object.talklike_set.filter(user=self.request.user).exists():
+            context['is_user_liked_for_talk'] = True
+        else:
+            context['is_user_liked_for_talk'] = False
+
+        return context
+
+class TalkCreateView(LoginRequiredMixin, CreateView):
+    model = Talk
+    fields = ['content']
+    template_name = 'talk_create.html'
+    success_url = reverse_lazy('MyApp:talk')
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+@require_POST
+@login_required
+def talk_like(request):
+    talk_pk = request.POST.get('talk_pk')
+    context = {
+        'user': f'{ request.user }',
+    }
+    talk = get_object_or_404(Talk, pk=talk_pk)
+    like = TalkLike.objects.filter(talk=talk, user=request.user)
+
+    if like.exists():
+        like.delete()
+        context['method'] = 'delete'
+    else:
+        like.create(talk=talk, user=request.user)
+        context['method'] = 'create'
+
+    context['talklike_count'] = talk.talklike_set.count()
+
+    return JsonResponse(context)
+
+#----------------MyPage----------------------
+
+class MyPageView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'myPage.html'
+    context_object_name = 'myPosts'
+    
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('MyApp:index')
+    template_name = 'signup.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # ユーザーが作成された場合はログインする
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return response
     
 
 def login_signup(request):

668d82843f1d1c364da1b4d416588b7a41eb1d93
kodakk <20fi039@gmail.com>
2023-03-30 23:27:18 +0900

    talk_like/を追加

--- MySNS/MyApp/urls.py
+++ MySNS/MyApp/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import RedirectView
 from django.contrib.auth.views import LoginView, LogoutView
 
 from .views import UserPostListView, TalkView, PostView, MyPageView, PostDetailView, TalkDetailView, SignupView, PostCreateView, TalkCreateView
-from .views import post_like, login_signup
+from .views import post_like, talk_like, login_signup
 
 app_name = 'MyApp'
 
@@ -15,6 +15,7 @@ urlpatterns = [
     path('talk/', TalkView.as_view(), name="talk"),
     path('talk/create', TalkCreateView.as_view(), name="talk_create"),
     path('talk/<int:pk>/', TalkDetailView.as_view(), name='talk_detail'),
+    path('talk_like/', talk_like, name='talk_like'),
 
     path('post/', PostView.as_view(), name="post"),
     path('post/create', PostCreateView.as_view(), name="post_create"),
